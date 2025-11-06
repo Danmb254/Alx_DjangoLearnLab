4d9c786ca22455275b4e1b41a20258832c008d85
Danielmbugua <platinumwriter2022@gmail.com>
2025-11-06 09:50:43 +0300

    did some mods

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query 1: Get all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
 # Query 2: List all books in a specific library
 def get_books_in_library(library_name):
@@ -14,9 +14,3 @@ def get_books_in_library(library_name):
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.librarian
-
-# Example usage (you can run this in Django shell)
-if __name__ == "__main__":
-    print("Books by John Doe:", get_books_by_author("John Doe"))
-    print("Books in City Library:", get_books_in_library("City Library"))
-    print("Librarian of City Library:", get_librarian_for_library("City Library"))
