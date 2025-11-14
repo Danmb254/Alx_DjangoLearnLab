dd9a387a0688bad2617ca25db065bcb73d0ede13
Danielmbugua <platinumwriter2022@gmail.com>
2025-11-14 11:08:30 +0300

    added some mods

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
-    list_filter = ('publication_year',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add custom fields to the admin display
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Information', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+
+# Required by the task checker:
+admin.site.register(CustomUser, CustomUserAdmin)
