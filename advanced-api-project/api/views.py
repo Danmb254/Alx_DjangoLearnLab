54826b52eb08ea9347ff6d2c624fab76d2e4aea4
Danielmbugua <platinumwriter2022@gmail.com>
2025-11-30 20:40:40 +0300

    add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -10,13 +10,10 @@ from .serializers import BookSerializer
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
 
-    # The checker NEEDS this exact line ↓
-    filter_backends = [DjangoFilterBackend, SearchFilter, filters.OrderingFilter]
-
-    filterset_fields = ['title', 'publication_year', 'author']
-    search_fields = ['title', 'author__name']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']
 
