2b72f0747ebd1ef715a9ee990dc3e8c8d7722ffe
Danielmbugua <platinumwriter2022@gmail.com>
2025-12-07 09:43:06 +0300

    add

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,7 @@
 # blog/forms.py
 from django import forms
+from .models import Post
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -9,3 +11,7 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
