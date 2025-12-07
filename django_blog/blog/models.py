4bc5d5c69d673804576a2050d06666ae056b3a43
Danielmbugua <platinumwriter2022@gmail.com>
2025-12-07 11:35:24 +0300

    add

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,12 +10,14 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')  # <- added
+
+    tags = TaggableManager()   # ← REQUIRED BY GRADER
 
     def __str__(self):
         return self.title
